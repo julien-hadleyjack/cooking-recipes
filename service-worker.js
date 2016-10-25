10a529ec9a0eb26553aef932dbc22b7b38297b2b
Travis-CI <github@jlhj.de>
2016-10-25 10:46:03 +0000

    Generated Jekyll Site by Travis CI - 40

---
+++ service-worker.js
@@ -0,0 +1,162 @@
+
+
+var urlsToCache = [];
+
+urlsToCache.push("/cooking-recipes/404.html");
+urlsToCache.push("/cooking-recipes/feed.xml");
+urlsToCache.push("/cooking-recipes/");
+urlsToCache.push("/cooking-recipes/assets/css/main.css");
+urlsToCache.push("/cooking-recipes/service-worker.js");
+
+
+urlsToCache.push("/cooking-recipes/20-minuten-sind-genug/nudeln-mit-linsen.html");
+urlsToCache.push("/cooking-recipes/suppen/moehrensuppe-mit-orangen-ingwer-und-koriander.html");
+urlsToCache.push("/cooking-recipes/one-pot-pasta/spaghetti-mit-erbsen-und-minz-gremolata.html");
+urlsToCache.push("/cooking-recipes/one-pot-pasta/gruene-gemuese-pasta-mit-zitronensauce.html");
+urlsToCache.push("/cooking-recipes/one-pot-pasta/fenchel-safran-nudeln-mit-huhn.html");
+urlsToCache.push("/cooking-recipes/one-pot-pasta/apfel-nuss-penne-mit-rosinen.html");
+urlsToCache.push("/cooking-recipes/one-pot-pasta/paprika-pasta-mit-feta-und-walnuessen.html");
+urlsToCache.push("/cooking-recipes/one-pot-pasta/kokos-tomaten-mit-spinat.html");
+urlsToCache.push("/cooking-recipes/one-pot-pasta/curry-nudeln-mit-blumenkohl.html");
+urlsToCache.push("/cooking-recipes/one-pot-pasta/pasta-alla-vesuviana.html");
+urlsToCache.push("/cooking-recipes/one-pot-pasta/grundrezept-pasta.html");
+urlsToCache.push("/cooking-recipes/one-pot-pasta/spaghetti-mit-zwiebeln-und-rindfleisch.html");
+urlsToCache.push("/cooking-recipes/one-pot-pasta/girandole-ai-funghi-con-pancetta.html");
+urlsToCache.push("/cooking-recipes/one-pot-pasta/brokkoli-nudeln-mit-schweinefilet.html");
+urlsToCache.push("/cooking-recipes/lunch-im-glas/scharfer-suesskartoffelsalat-mit-bohnen.html");
+urlsToCache.push("/cooking-recipes/lunch-im-glas/roh-marinierter-fenchelsalat-mit-salami.html");
+urlsToCache.push("/cooking-recipes/natuerlich-essen/wuerzige-moehrensuppe-mit-kokosmilch.html");
+urlsToCache.push("/cooking-recipes/natuerlich-essen/rote-linsensuppe-mit-spinat-und-zitrone.html");
+urlsToCache.push("/cooking-recipes/natuerlich-essen/curry.html");
+urlsToCache.push("/cooking-recipes/basic-cooking-2/schnitzel-wok.html");
+urlsToCache.push("/cooking-recipes/basic-cooking-2/brokkolicreme-mit-geroesteten-pinienkernen.html");
+urlsToCache.push("/cooking-recipes/basic-cooking-2/chicken-tortilla-wraps.html");
+urlsToCache.push("/cooking-recipes/basic-cooking-2/steak-sandwich.html");
+urlsToCache.push("/cooking-recipes/basic-cooking/gebratene-asiatische-nudeln.html");
+urlsToCache.push("/cooking-recipes/crashkurs-kochen/schweinemedaillons-in-pilzsahne.html");
+urlsToCache.push("/cooking-recipes/crashkurs-kochen/asiatischer-wei%C3%9Fkohlsalat.html");
+urlsToCache.push("/cooking-recipes/crashkurs-kochen/griechicher-salat-mit-tomatensauce.html");
+urlsToCache.push("/cooking-recipes/crashkurs-kochen/paprika-reispfanne.html");
+urlsToCache.push("/cooking-recipes/crashkurs-kochen/gemueseeintopf-mit-reis.html");
+urlsToCache.push("/cooking-recipes/crashkurs-kochen/gebratene-nudeln-mit-anas-und-pute.html");
+urlsToCache.push("/cooking-recipes/crashkurs-kochen/bohnen-tagliatelle-mit-pesto.html");
+urlsToCache.push("/cooking-recipes/crashkurs-kochen/ravioli-in-kaese-zitronen-sauce.html");
+urlsToCache.push("/cooking-recipes/20-minuten-sind-genug/ungarische-reispfanne.html");
+urlsToCache.push("/cooking-recipes/20-minuten-sind-genug/marseiller-bohnentopf.html");
+urlsToCache.push("/cooking-recipes/20-minuten-sind-genug/cajun-gemuesepfanne.html");
+
+
+urlsToCache.push("/cooking-recipes/assets/fonts/FontAwesome.otf");
+urlsToCache.push("/cooking-recipes/assets/fonts/fontawesome-webfont.eot");
+urlsToCache.push("/cooking-recipes/assets/fonts/fontawesome-webfont.svg");
+urlsToCache.push("/cooking-recipes/assets/fonts/fontawesome-webfont.ttf");
+urlsToCache.push("/cooking-recipes/assets/fonts/fontawesome-webfont.woff");
+urlsToCache.push("/cooking-recipes/assets/fonts/fontawesome-webfont.woff2");
+urlsToCache.push("/cooking-recipes/assets/js/main.min.js");
+urlsToCache.push("/cooking-recipes/images/apfel-nuss-penne-mit-rosinen.jpg");
+urlsToCache.push("/cooking-recipes/images/asiatischer-weißkohlsalat.jpg");
+urlsToCache.push("/cooking-recipes/images/bohnen-tagliatelle-mit-pesto-2.jpg");
+urlsToCache.push("/cooking-recipes/images/bohnen-tagliatelle-mit-pesto.jpg");
+urlsToCache.push("/cooking-recipes/images/brokkoli-nudeln-mit-schweinefilet.jpg");
+urlsToCache.push("/cooking-recipes/images/brokkolicreme-mit-geroesteten-pinienkernen.jpg");
+urlsToCache.push("/cooking-recipes/images/cajun-gemuesepfanne.jpg");
+urlsToCache.push("/cooking-recipes/images/curry-2.jpg");
+urlsToCache.push("/cooking-recipes/images/curry-nudeln-mit-blumenkohl.jpg");
+urlsToCache.push("/cooking-recipes/images/curry.jpg");
+urlsToCache.push("/cooking-recipes/images/default-teaser.png");
+urlsToCache.push("/cooking-recipes/images/fenchel-safran-nudeln-mit-huhn.jpg");
+urlsToCache.push("/cooking-recipes/images/gebratene-asiatische-nudeln.jpg");
+urlsToCache.push("/cooking-recipes/images/gemueseeintopf-mit-reis.jpg");
+urlsToCache.push("/cooking-recipes/images/girandole-ai-funghi-con-pancetta.jpg");
+urlsToCache.push("/cooking-recipes/images/griechicher-salat-mit-tomatensauce.jpg");
+urlsToCache.push("/cooking-recipes/images/gruene-gemuese-pasta-mit-zitronensauce.jpg");
+urlsToCache.push("/cooking-recipes/images/grundrezept-pasta.jpg");
+urlsToCache.push("/cooking-recipes/images/kokos-tomaten-mit-spinat.jpg");
+urlsToCache.push("/cooking-recipes/images/marseiller-bohnentopf.jpg");
+urlsToCache.push("/cooking-recipes/images/moehrensuppe-mit-orangen-ingwer-und-koriander.jpg");
+urlsToCache.push("/cooking-recipes/images/nudeln-mit-linsen.jpg");
+urlsToCache.push("/cooking-recipes/images/paprika-pasta-mit-feta-und-walnuessen.jpg");
+urlsToCache.push("/cooking-recipes/images/paprika-reispfanne.jpg");
+urlsToCache.push("/cooking-recipes/images/pasta-alla-vesuviana.jpg");
+urlsToCache.push("/cooking-recipes/images/roh-marinierter-fenchelsalat-mit-salami.jpg");
+urlsToCache.push("/cooking-recipes/images/scharfer-suesskartoffelsalat-mit-bohnen.jpg");
+urlsToCache.push("/cooking-recipes/images/schweinemedaillons-in-pilzsahne-2.jpg");
+urlsToCache.push("/cooking-recipes/images/schweinemedaillons-in-pilzsahne.jpg");
+urlsToCache.push("/cooking-recipes/images/spaghetti-mit-erbsen-und-minz-gremolata.jpg");
+urlsToCache.push("/cooking-recipes/images/spaghetti-mit-zwiebeln-und-rindfleisch.jpg");
+urlsToCache.push("/cooking-recipes/images/steak-sandwich.jpg");
+urlsToCache.push("/cooking-recipes/images/thumbnail/apfel-nuss-penne-mit-rosinen.jpg");
+urlsToCache.push("/cooking-recipes/images/thumbnail/asiatischer-weißkohlsalat.jpg");
+urlsToCache.push("/cooking-recipes/images/thumbnail/bohnen-tagliatelle-mit-pesto-2.jpg");
+urlsToCache.push("/cooking-recipes/images/thumbnail/bohnen-tagliatelle-mit-pesto.jpg");
+urlsToCache.push("/cooking-recipes/images/thumbnail/brokkoli-nudeln-mit-schweinefilet.jpg");
+urlsToCache.push("/cooking-recipes/images/thumbnail/brokkolicreme-mit-geroesteten-pinienkernen.jpg");
+urlsToCache.push("/cooking-recipes/images/thumbnail/cajun-gemuesepfanne.jpg");
+urlsToCache.push("/cooking-recipes/images/thumbnail/curry-2.jpg");
+urlsToCache.push("/cooking-recipes/images/thumbnail/curry-nudeln-mit-blumenkohl.jpg");
+urlsToCache.push("/cooking-recipes/images/thumbnail/curry.jpg");
+urlsToCache.push("/cooking-recipes/images/thumbnail/default-teaser.png");
+urlsToCache.push("/cooking-recipes/images/thumbnail/fenchel-safran-nudeln-mit-huhn.jpg");
+urlsToCache.push("/cooking-recipes/images/thumbnail/gebratene-asiatische-nudeln.jpg");
+urlsToCache.push("/cooking-recipes/images/thumbnail/gemueseeintopf-mit-reis.jpg");
+urlsToCache.push("/cooking-recipes/images/thumbnail/girandole-ai-funghi-con-pancetta.jpg");
+urlsToCache.push("/cooking-recipes/images/thumbnail/griechicher-salat-mit-tomatensauce.jpg");
+urlsToCache.push("/cooking-recipes/images/thumbnail/gruene-gemuese-pasta-mit-zitronensauce.jpg");
+urlsToCache.push("/cooking-recipes/images/thumbnail/grundrezept-pasta.jpg");
+urlsToCache.push("/cooking-recipes/images/thumbnail/kokos-tomaten-mit-spinat.jpg");
+urlsToCache.push("/cooking-recipes/images/thumbnail/marseiller-bohnentopf.jpg");
+urlsToCache.push("/cooking-recipes/images/thumbnail/moehrensuppe-mit-orangen-ingwer-und-koriander.jpg");
+urlsToCache.push("/cooking-recipes/images/thumbnail/nudeln-mit-linsen.jpg");
+urlsToCache.push("/cooking-recipes/images/thumbnail/paprika-pasta-mit-feta-und-walnuessen.jpg");
+urlsToCache.push("/cooking-recipes/images/thumbnail/paprika-reispfanne.jpg");
+urlsToCache.push("/cooking-recipes/images/thumbnail/pasta-alla-vesuviana.jpg");
+urlsToCache.push("/cooking-recipes/images/thumbnail/roh-marinierter-fenchelsalat-mit-salami.jpg");
+urlsToCache.push("/cooking-recipes/images/thumbnail/scharfer-suesskartoffelsalat-mit-bohnen.jpg");
+urlsToCache.push("/cooking-recipes/images/thumbnail/schweinemedaillons-in-pilzsahne-2.jpg");
+urlsToCache.push("/cooking-recipes/images/thumbnail/schweinemedaillons-in-pilzsahne.jpg");
+urlsToCache.push("/cooking-recipes/images/thumbnail/spaghetti-mit-erbsen-und-minz-gremolata.jpg");
+urlsToCache.push("/cooking-recipes/images/thumbnail/spaghetti-mit-zwiebeln-und-rindfleisch.jpg");
+urlsToCache.push("/cooking-recipes/images/thumbnail/steak-sandwich.jpg");
+urlsToCache.push("/cooking-recipes/images/thumbnail/ungarische-reispfanne.jpg");
+urlsToCache.push("/cooking-recipes/images/ungarische-reispfanne.jpg");
+
+
+var CACHE_NAME = 'cooking-by-the-book-cache-v1';
+
+self.addEventListener('install', function(event) {
+  // Perform install steps
+  event.waitUntil(caches.open(CACHE_NAME).then(function(cache) {
+    return cache.addAll(urlsToCache);
+  }).catch(function(err) {
+    console.log('cache add err', err);
+  }));
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.match(event.request).then(function (response) {
+        return response || fetch(event.request).then(function(response) {
+          cache.put(event.request, response.clone());
+          return response;
+        });
+      });
+    })
+  );
+});
+
+// strategies from the offline cookbook by jake archibald
+// https://jakearchibald.com/2014/offline-cookbook/#serving-suggestions-responding-to-requests
+
+
+
+  self.addEventListener('fetch', function(event) {
+    event.respondWith(
+      caches.open(CACHE_NAME).then(function(cache) {
+        return fetch(event.request).then(function(response) {
+          cache.put(event.request, response.clone());
+          return response;
+        });
+      })
+    );
+  });
