3161baa391b39e41bdf8952b51126e2ac310a166
Julien Hadley Jack <gitkraken@jlhj.de>
2016-10-30 18:20:51 +0100

    added favicons
    needed to modify create_thumbnails.rb to ignore favicon directory

--- _plugins/create_thumbnails.rb
+++ _plugins/create_thumbnails.rb
@@ -10,19 +10,19 @@ module Jekyll
   class ThumbGenerator < Generator
 
     def generate(site)
-      prefix = 'thumbnail'
+      thumb_dir = 'thumbnail'
+      ignored_dirs = %w(thumbnail favicon)
       width = 500
       height = 300
 
       asset_root = File.join site.source, 'images'
-      reject = File.join asset_root, prefix
 
 
       Find.find(asset_root).each do |file|
         rel_path = Pathname.new(file).relative_path_from(Pathname.new(asset_root))
-        thumb_file = File.join site.source, 'images', prefix, rel_path
+        thumb_file = File.join site.source, 'images', thumb_dir, rel_path
 
-        if file.start_with?(reject) or !FileMagic.new(FileMagic::MAGIC_MIME_TYPE).file(file).include?('image/') or
+        if check_dir(asset_root, ignored_dirs, file) or !FileMagic.new(FileMagic::MAGIC_MIME_TYPE).file(file).include?('image/') or
             (File.exists?(thumb_file) and File.mtime(thumb_file) > File.mtime(file))
           next
         end
@@ -42,12 +42,20 @@ module Jekyll
         image.destroy!
 
         site.static_files << Jekyll::StaticFile.new(site, site.source,
-                                                    File.dirname(File.join('images', prefix, rel_path)),
+                                                    File.dirname(File.join('images', thumb_dir, rel_path)),
                                                     File.basename(thumb_file))
       end
 
     end
 
-end
+    def check_dir(asset_root, ignored_dirs, path)
+      if ignored_dirs.is_a?(Array)
+        ignored_dirs.any? { |dir| path.start_with?(File.join(asset_root, dir)) }
+      else
+        path.start_with?(File.join(asset_root, ignored_dirs))
+      end
+    end
+
+  end
 
 end
